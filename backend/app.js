228da3ee76a37062856e234878e23e60c902b7a5
Fayiz <fayizcj94.com>
2024-02-12 23:40:17 +0530

    First commit

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const port = 5000
+
+
+const app = express();
+
+app.get('/', (req,res)=> res. send ('Server is Running'))
+
+app.listen(port, () =>{
+    console.log(`Server started on port ${port}`);
+})
