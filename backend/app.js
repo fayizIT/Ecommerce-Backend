14749924252ce016cf4423a2da77b674f5e810bb
Fayiz <fayizcj94.com>
2024-02-13 14:43:37 +0530

    add token in  admin side

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js'
+import adminRoutes from './routes/adminRoutes.js'
 import { notFound, errorHandler} from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
@@ -15,11 +17,16 @@ connectDB()
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors({
+    origin:"*"
+  }))
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
 app.use('/api/users',userRoutes)
+app.use('/api/admin',adminRoutes)
 
 app.get('/', (req,res)=> res. send ('Server is Running'))
 
