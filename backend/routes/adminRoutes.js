ce9ed0fca801e3d709fe30602f1175bf94ecf968
Fayiz <fayizcj94.com>
2024-02-13 13:01:07 +0530

    create admin auth middlewere

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,11 +1,47 @@
 import express from 'express';
+import multer from "multer";
+import path from "path";
 import { registerAdmin, loginAdmin, logoutAdmin, } from '../controller/adminController.js';
+
+
+import { adminProtect } from '../middleware/adminAuthMiddleware.js';
+
+
+
+//Multer setup
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+      cb(null, 'backend/public')
+    },
+    filename:(req,file,cb) => {
+      cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
+    }
+  })
+  const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only images are allowed!"), false); 
+    }
+  };
+
+  const upload = multer({ 
+    storage: storage,
+    fileFilter: fileFilter, 
+  });
 
 
 
 router.post('/',registerAdmin)
 router.post('/auth', loginAdmin)
 router.post('/logout',logoutAdmin)
+
+router.post('/add-product', adminProtect, upload.single('file'), (req, res, next) => {
+    if (req.fileValidationError) {
+        return res.status(400).json({ error: req.fileValidationError });
+    }
+    next();
+  }, addProduct);
 
 
 
