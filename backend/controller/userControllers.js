65a3be36bd26144ee0ee74d2128e43c88a79b6de
Fayiz <fayizcj94.com>
2024-02-13 12:32:11 +0530

    user authetication

--- backend/controller/userControllers.js
+++ backend/controller/userControllers.js
@@ -64,8 +64,31 @@ const loginUser = asyncHandler(async (req, res) => {
     }
   });
 
+
+
+  const getUserProfile = asyncHandler(async (req, res) => {
+    const user = {
+      id: req.user._id,
+      name: req.user.name,
+      email: req.user.email,
+    };
+    res.status(200).json(user);
+  });
+
+  
+
+  const logoutUser = asyncHandler(async (req, res) => {
+    res.cookie("jwt", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    res.status(200).json({ message: "User logged out" });
+  });
+
   export { 
     registerUser,
-    loginUser
+    loginUser,
+    getUserProfile,
+    logoutUser
 
   };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 const router = express.Router();
 
-import { loginUser, registerUser} from '../controller/userControllers.js';
+import { loginUser, registerUser, getUserProfile,logoutUser} from '../controller/userControllers.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 
 router.post('/',registerUser)
-router.post('/auth', loginUser)
+router.post('/login', loginUser)
+router.get('/profile',protect, getUserProfile);
+router.post('/logout',logoutUser)
 
 
 
