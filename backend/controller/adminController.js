9fb4be54974c174fc0dfe859c8544d568523ef16
Fayiz <fayizcj94.com>
2024-02-13 12:54:27 +0530

    create controller authetication

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -1,85 +1,68 @@
-import asyncHandler from "express-async-handler";
-import Admin from "../models/adminModel.js";
-import generateTokenAdmin from "../utils/generateTokenAdmin.js";
+import asyncHandler from 'express-async-handler';
+import Admin from '../models/adminModel.js';
 
+const generateAdminResponse = (res, admin) => {
+    generateTokenAdmin(res, admin._id);
+    res.status(201).json({
+        _id: admin._id,
+        name: admin.name,
+        email: admin.email,
+    });
+};
 
-//Admin login
-const loginAdmin = asyncHandler( async(req,res)=>{
-    const {email,password}= req.body;
+// Admin login
+const loginAdmin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
-    const admin = await Admin.findOne({email});
+    const admin = await Admin.findOne({ email });
 
-    if(admin && (await admin.matchPaswword(password))){
-        generateTokenAdmin(res,admin._id)
-        res.status(201).json({
-            _id:admin._id,
-            name:admin.name,
-            email:admin.email
-        })
-    }else{
+    if (admin && (await admin.matchPaswword(password))) {
+        generateAdminResponse(res, admin);
+    } else {
         res.status(400);
-        throw new Error('invalid email or password')
+        throw new Error('Invalid email or password');
     }
-
-
 });
 
+// Register Admin
+const registerAdmin = asyncHandler(async (req, res) => {
+    const { name, email, password, key } = req.body;
 
-//Register Admin
+    const adminExist = await Admin.findOne({ email });
 
-const registerAdmin = asyncHandler( async(req,res)=>{
-    const {name,email,password,key} = req.body
-
-    const adminExist = await Admin.findOne({email});
-
-    if(adminExist){
+    if (adminExist) {
         res.status(400);
-        throw new Error('Admin already exisit')
+        throw new Error('Admin already exists');
     }
-    console.log(key,'keyyyyyyyyyy')
-    if(key!== process.env.ADMIN_KEY){
+
+    console.log(key, 'key');
+
+    if (key !== process.env.ADMIN_KEY) {
         res.status(401);
-        throw new Error('Invalid Key')
+        throw new Error('Invalid Key');
     }
 
     const admin = await Admin.create({
         name,
         email,
-        password
-    })
+        password,
+    });
 
-    
-
-    if(admin){
-        generateTokenAdmin(res,admin._id)
-        res.status(201).json({
-            _id:admin._id,
-            name:admin.name,
-            email:admin.email
-        })
-    }else{
+    if (admin) {
+        generateAdminResponse(res, admin);
+    } else {
         res.status(400);
-        throw new Error('invalid admin data')
+        throw new Error('Invalid admin data');
     }
-    
 });
 
-
-// Loggin out admin
-
-const logoutAdmin = asyncHandler( async(req,res)=>{
-    res.cookie('adminJwt','',{
-        httpOnly:true,
-        expires:new Date(0)
-    })
-    res.status(200).json({message:"Logged out"})
+// Logout Admin
+const logoutAdmin = asyncHandler(async (req, res) => {
+    res.cookie('adminJwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+    res.status(200).json({ message: 'Logged out' });
 });
 
-
-
-
-export {
-    loginAdmin,
-    registerAdmin,
-    logoutAdmin,
-}
+export { loginAdmin, registerAdmin, logoutAdmin };
