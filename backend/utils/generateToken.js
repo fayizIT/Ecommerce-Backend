3a3c1de50f54a3b401751302f1c0e576cdbd4329
Fayiz <fayizcj94.com>
2024-02-19 19:08:38 +0530

    uopdates in utils

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,15 +1,16 @@
 import jwt from 'jsonwebtoken';
 
+const generateToken = (res, userId) => {
+  // Generate the JWT token with the user ID and sign it with the secret key
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
-const generateToken=(res,userId)=>{
-    const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:'30d'})
-
-    res.cookie('jwt',token,{
-        httpOnly : true,
-        secure : process.env.NODE_ENV !== 'development',
-        sameSite : 'strict',
-        maxAge : 30 * 24 * 60 * 60 * 1000 // it calculates in seconds so the seconds converting to 30 days
-    })
-}
+  // Set the token in a cookie with specified options
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development', // Set to true in production
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+  });
+};
 
 export default generateToken;
--- backend/utils/generateTokenAdmin.js
+++ backend/utils/generateTokenAdmin.js
@@ -1,15 +1,17 @@
 import jwt from 'jsonwebtoken';
 
+const generateTokenAdmin = (res, adminId) => {
+  // Generate the JWT token with the admin ID and sign it with the secret key
+  const token = jwt.sign({ adminId }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
-const generateTokenAdmin=(res,adminId)=>{
-    const token = jwt.sign({adminId},process.env.JWT_SECRET,{expiresIn:'30d'})
-
-    res.cookie('adminJwt',token,{
-        httpOnly : true,
-        secure : process.env.NODE_ENV !== 'devolopment',
-        sameSite : 'strict',
-        maxAge : 30 * 24 * 60 * 60 * 1000 // it calculates in seconds so the seconds converting to 30 days
-    })
-}
+  // Set the token in a cookie with specified options
+  res.cookie('adminJwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development', // Set to true in production
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+  });
+};
 
 export default generateTokenAdmin;
+
