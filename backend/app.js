d9c6aaf715af2b6ba5742907f00c99052025cc72
Fayiz <fayizcj94.com>
2024-02-21 20:34:41 +0530

    Final commit

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import adminRoutes from './routes/adminRoutes.js'
+import { fileURLToPath } from 'url';
+import path from 'path';
 dotenv.config();
 
 connectDB()
@@ -13,13 +15,16 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
 app.use(cors({
     origin:"*"
   }))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
-app.use(express.static('backend/public'));
+app.use('/golbalSearch/products', express.static(path.join(__dirname, '/public')));
 
 app.use('/api/users',userRoutes)
 app.use('/api/admin',adminRoutes)
